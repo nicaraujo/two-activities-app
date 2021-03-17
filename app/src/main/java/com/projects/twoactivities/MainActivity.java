2f93bda24344129b90534b80af265b5aee1c2b93
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-17 14:31:52 -0300

    Send data to second activity and return data back logic added

--- app/src/main/java/com/projects/twoactivities/MainActivity.java
+++ app/src/main/java/com/projects/twoactivities/MainActivity.java
@@ -1,25 +1,51 @@
 package com.projects.twoactivities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.projects.twoactivities.extra.MESSAGE";
+    private EditText editText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView textView;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText_main);
+        textView = findViewById(R.id.text_header_reply);
+        textView2 = findViewById(R.id.text_reply);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button Clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                textView.setVisibility(View.VISIBLE);
+                textView2.setText(reply);
+                textView2.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
